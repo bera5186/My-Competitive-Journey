97cd220677d64717abf1a3cf792246fd412bbd69
bera5186 <rbasu611@gmail.com>
2018-10-13 23:54:01 +0530

    1C

---
+++ CodeForces/1C-AncientCircus.cpp
@@ -0,0 +1,13 @@
+/*
+This problem comprises with finding area of  a triangle given its coordinates
+approach : i will use heron's formula for this
+read more about this from this link https://en.wikipedia.org/wiki/Heron%27s_formula
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float x1,y1,x2,y2,x3,y3,a,b,c;
+
+    return 0;
+}
