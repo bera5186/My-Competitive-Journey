51a9a05439e04c986b88a0ffb9ed2e3600e58c90
bera5186 <rbasu611@gmail.com>
2018-10-22 19:45:31 +0530

    Fermat-little-theorem

---
+++ ALGOS FOR CP/PRIMILITY-TESTS/Fermat's-method.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int power(int a, int n){
+    return pow(a,n);
+}
+int main(){
+    int n=9;
+    bool x;
+    x = n&1;
+    //cout<<x;
+
+    cout<<pow(2,3);
+    return 0;
+
+}
