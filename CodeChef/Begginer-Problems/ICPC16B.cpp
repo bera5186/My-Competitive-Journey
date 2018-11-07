bf0014fa92929e225c9db1b77ac4d4775e02a90d
bera5186 <rbasu611@gmail.com>
2018-11-07 13:19:30 +0530

    Begginer Problems on Codechef

---
+++ CodeChef/Begginer-Problems/ICPC16B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t,n;
+    vector<int>a(100000);
+    cin>>t;
+    while(t--){
+        
+    }
+}
