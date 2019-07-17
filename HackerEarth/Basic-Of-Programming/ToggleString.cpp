dac7abea6b4f5b764541cfe74f08bd8da7119838
bera5186 <rbasu611@gmail.com>
2019-07-17 13:40:55 +0530

    AC: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/modify-the-string/

---
+++ HackerEarth/Basic-Of-Programming/ToggleString.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (int i =0 ; i < s.length(); i++)
+    {
+        if(isupper(s[i])) s[i] = tolower(s[i]) ;
+        else s[i] = toupper(s[i]) ;
+    }
+    cout << s;
+
+    return 0;
+}
